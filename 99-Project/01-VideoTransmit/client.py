ecac76c45d91d96010e8315fe181bda9a246b6e5
andrew chu <andrew.chu@schindler.com>
2019-07-26 10:12:04 +0800

    auto deploy

---
+++ 99-Project/01-VideoTransmit/client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/26 10:11
+# @Author  : Andrew.chu
+# @Site    : Schindler
+# @File    : client.py
+# @Software: PyCharm
+'''
+Description:
+
+'''
