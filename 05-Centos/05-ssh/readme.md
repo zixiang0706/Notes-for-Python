efb356a8bfae7b898683bf7a2b492acbfb7aeaee
andrew chu <andrew.chu@schindler.com>
2019-08-21 09:46:59 +0800

    "add"

--- 05-Centos/05-ssh/readme.md
+++ 05-Centos/05-ssh/readme.md
@@ -1,12 +1,13 @@
-# ssh
+﻿# ssh
 
-
+vim /etc/rc.local
 
 ## usefull notes
 首先要安装openssh
 开启ssh服务 /usr/sbin/sshd
 　　// ubuntu系统
 　　service ssh restart
+	systemctl reload sshd
 
 　　// debian系统
 　　/etc/init.d/ssh restart
@@ -17,5 +18,15 @@ ssh-copy-id user@host
 
 ssh user@host 'mkdir -p .ssh && cat >> .ssh/authorized_keys' < ~/.ssh/id_rsa.pub
 
+@at home
 in host1, run: ssh -fNgL 2121:$host2:22 $host3
 in host2, run:  ssh -NfgR 10030:$host2:22 root@47.96.174.116
+
+@open service
+ssh -o ServerAliveInterval=60 -NgfR 10030:192.168.8.102:22 root@47.96.174.116
+ssh -NgfL 10031:47.96.174.116:22 root@47.96.174.116
+@start service
+ssh -p 10030 root@47.96.174.116 ##enter the passwd of Pi
+
+
+sudo lsb_release -a 查看版本
--- 99-Project/01-VideoTransmit/client.py
+++ 99-Project/01-VideoTransmit/client.py
@@ -1,11 +1,70 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2019/7/26 10:11
-# @Author  : Andrew.chu
-# @Site    : Schindler
-# @File    : client.py
-# @Software: PyCharm
-'''
-Description:
+import socket
+import threading
+import struct
+import cv2
+import time
+import os
+import numpy
 
-'''
+class webCamera:
+    def __init__(self, resolution = (640, 480), host = ("", 7999)):
+        self.resolution = resolution
+        self.host = host
+        self.setSocket(self.host)
+        self.img_quality = 15
+
+    def setImageResolution(self, resolution):
+        self.resolution = resolution
+
+    def setHost(self, host):
+        self.host = host
+
+    def setSocket(self, host):
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        self.socket.bind(self.host)
+        self.socket.listen(5)
+        print("Server running on port:%d" % host[1])
+
+    def recv_config(self,client):
+        info = struct.unpack("lhh",client.recv(8))
+        if info[0]>911:        #print(info[0])
+            self.img_quality=int(info[0])-911
+            self.resolution=list(self.resolution)
+            self.resolution[0]=info[1]
+            self.resolution[1]=info[2]
+            self.resolution=tuple(self.resolution)
+            return 1
+        else :
+            return 0
+
+    def _processConnection(self, client,addr):
+        if(self.recv_config(client)==0):
+            return
+        camera = cv2.VideoCapture(0)
+        encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),self.img_quality]
+        while(1):
+            time.sleep(0.04)
+            (grabbed, self.img) = camera.read()
+            self.img  = cv2.resize(self.img,self.resolution)
+            result, imgencode = cv2.imencode('.jpg',self.img,encode_param)
+            img_code = numpy.array(imgencode)
+            self.imgdata  = img_code.tostring()
+
+            client.send(struct.pack("lhh",len(self.imgdata),
+                    self.resolution[0],self.resolution[1])+self.imgdata) #发送图片信息(图片长度,分辨率,图片内容)
+
+
+    def run(self):
+        while(1):
+            client,addr = self.socket.accept()
+            clientThread = threading.Thread(target = self._processConnection,
+                args = (client, addr, ))  #有客户端连接时产生新的线程进行处理
+            clientThread.start()
+
+def main():
+    cam = webCamera()
+    cam.run()
+
+if __name__ == "__main__":
+    main()
